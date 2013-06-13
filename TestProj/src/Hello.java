d81d2b235c94a18b30e1cd4a479e069d6850ccdf
Michael Shemesh <mshem@brandeis.edu>
2013-06-13 13:25:58 -0400

    hello world

---
+++ TestProj/src/Hello.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Mike
+ *
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello, World");
+
+	}
+
+}
